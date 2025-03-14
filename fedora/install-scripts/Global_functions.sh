c7687a64a45e0bd8c903e1bacc62442cf76efc0d
AXWTV <azmainsharer@gmail.com>
2025-03-14 21:38:28 +0600

    fix handle multiple packages at once properly

--- fedora/install-scripts/Global_functions.sh
+++ fedora/install-scripts/Global_functions.sh
@@ -23,22 +23,22 @@ set -e
 
 # Function for installing packages
 install_package() {
-  # Checking if package is already installed
-  if sudo dnf list installed "$1" &>> /dev/null ; then
-    echo -e "${OK} $1 is already installed. Skipping..."
-  else
-    # Package not installed
-    echo -e "${NOTE} Installing $1 ..."
-    sudo dnf install -y "$1" 2>&1 | tee -a "$LOG"
-    # Making sure package is installed
-    if sudo dnf list installed "$1" &>> /dev/null ; then
-      echo -e "\e[1A\e[K${OK} $1 was installed."
+  for package in "$@"; do
+    # Checking if package is already installed
+    if sudo dnf list installed "$package" &>> /dev/null ; then
+      echo -e "${OK} $package is already installed. Skipping..."
     else
-      # Something is missing, exiting to review log
-      echo -e "\e[1A\e[K${ERROR} $1 failed to install :( , please check the install.log. You may need to install manually! Sorry I have tried :("
-      exit 1
+      # Package not installed
+      echo -e "${NOTE} Installing $package ..."
+      if sudo dnf install -y "$package" 2>&1 | tee -a "$LOG"; then
+        echo -e "\e[1A\e[K${OK} $package was installed."
+      else
+        # Something is missing, exiting to review log
+        echo -e "\e[1A\e[K${ERROR} $package failed to install :( , please check the install.log. You may need to install manually! Sorry I have tried :("
+        exit 1
+      fi
     fi
-  fi
+  done
 }
 
 # Function for uninstalling packages
