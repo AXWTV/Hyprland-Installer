71736e13af2ff4eb0b368a4cfd997bf3a70130d4
AXWTV <88997488+AXWTV@users.noreply.github.com>
2025-04-30 20:08:41 +0600

    Sudo privileges

--- fedora/install-scripts/00-ags-pkgs.sh
+++ fedora/install-scripts/00-ags-pkgs.sh
@@ -7,7 +7,7 @@ LOG="Install-Logs/install-$(date +%d-%H%M%S)_ags-pkgs.log"
 if [ -d ~/.bun/ ]; then
     echo "Bun is already installed. Skipping..."
 else
-   curl -fsSL https://bun.sh/install | bash 2>&1 | tee -a "$LOG"
+   sudo curl -fsSL https://bun.sh/install | bash 2>&1 | tee -a "$LOG"
 fi
 
 # Sass
